7cd38ae9d2a58ce448510dbd586de2ec67da5b8e
sariahmghames <50996677+sariahmghames@users.noreply.github.com>
2023-01-26 16:41:31 +0000

    Update setup.py

--- NeuRoSyM-darnn/scripts/setup.py
+++ NeuRoSyM-darnn/scripts/setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['qtc_predict'],
-    package_dir={'': 'scripts'}
+    packages=['scripts'],
+    package_dir={'': 'src'}
 )
 
 setup(**setup_args)
